ec0b87fbe3365a29ff7d9078195933006e5dbf33
Danielle Palmer <daniellezpalmer@gmail.com>
2017-10-14 13:54:06 -0500

    Finished problems 7 & 8 using iteration

--- main.js
+++ main.js
@@ -89,6 +89,10 @@ let letterVals = [ "v", "x", "r", "f", "p" ];
 function interleave(){
     let vals = [];
     // Your answer here:
+    for ( let i = 0; i < numberVals.length; i++){
+        vals.push(letterVals[i] + numberVals[i]);
+    }
+    return vals;
 }
 
 
@@ -106,4 +110,9 @@ let second = [ "think", "stand", "cheese", "break", "numinous", "mouse", "close"
 function union(){
     let same = [];
     // Your answer here:
+    for (var i = 0; i < first.length; i++) {
+     if (first[i] === second[i]){
+       same.push(first[i]);
+     }
+   } return same;
 }
